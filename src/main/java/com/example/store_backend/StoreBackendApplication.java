a738562e157409a61fb50f559fa97acbd9e2ca17
santhoshkumarpv002 <santhoshkumarpv002@gmail.com>
2025-06-25 14:11:34 +0000

    initialized

---
+++ src/main/java/com/example/store_backend/StoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreBackendApplication.class, args);
+	}
+
+}
